260597ed096a665b9737d7a36e53b120d8df9857
oliver laleau <laleaumix@outlook.com>
2022-01-25 02:31:50 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// https://cdn.dribbble.com/users/322058/screenshots/4342280/everdo_dribbble.png
+
+// const button = document.getElementById("click")
+
+// var counter = 0
+// button.addEventListener("click", () => {
+//     const item = document.getElementById("today")
+//     const x = document.createElement("li")
+//     x.textContent = counter.toString()
+//     x.id = "item"
+//     counter++
+//     item.appendChild(x)
+
+// })
+
+// const task = document.getElementById("item")
+
+// task.addEventListener("change", (event) => {
+//     const result = document.querySelector("li")
+//     result.textContent = `${event.target.value}`
+//   })
+  
